b8807d3ca5afed0a0063b93f2e0e8b815c639a89
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-07 18:35:50 -0500

    commit - Conexion / .

--- src/main/java/com/ejemplo/database/Conexion.java
+++ src/main/java/com/ejemplo/database/Conexion.java
@@ -1,4 +1,29 @@
 package com.ejemplo.database;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Conexion {
-}
+
+    private static final String URL =
+            "jdbc:mysql://localhost:3306/proyecto_excel";
+
+    private static final String USER = "root";
+    private static final String PASSWORD = "Tapiero123";
+
+    public static Connection obtenerConexion() {
+
+        try {
+            return DriverManager.getConnection(URL, USER, PASSWORD);
+
+        } catch (SQLException e) {
+
+            System.out.println("error de Conexion");
+
+            e.printStackTrace();
+
+            return null;
+        }
+    }
+}   
