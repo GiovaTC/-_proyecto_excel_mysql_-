d053e6f8093eeded4b2dcb31300746c0cdcd89e3
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-07 18:49:24 -0500

    commit - SumaDAO / .

--- src/main/java/com/ejemplo/database/SumaDAO.java
+++ src/main/java/com/ejemplo/database/SumaDAO.java
@@ -1,4 +1,59 @@
 package com.ejemplo.database;
 
+import com.ejemplo.model.Resultado;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.Statement;
+
 public class SumaDAO {
-}
+
+    public void guardar(Resultado resultado) {
+
+        String sql =
+                "INSERT INTO suma_filas(fila,suma) VALUES(?,?)";
+
+        try (
+                Connection conexion = Conexion.obtenerConexion();
+
+                PreparedStatement ps =
+                        conexion.prepareStatement(sql)
+
+        ) {
+            ps.setInt(1, resultado.getFila());
+            ps.setDouble(2, resultado.getSuma());
+
+            ps.executeUpdate();
+
+            System.out.println("registro Guardado!");
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void listar() {
+
+        String sql =
+                "SELECT * FROM suma_filas";
+
+        try (
+                Connection conexion = Conexion.obtenerConexion();
+                Statement st = conexion.createStatement();
+                ResultSet rs = st.executeQuery(sql)
+        ) {
+            System.out.println();
+            System.out.println("===== DATOS EN MYSQL =====");
+
+            while (rs.next()) {
+                System.out.println(
+                        "ID: " + rs.getInt("id")
+                                + "  Fila: " + rs.getInt("fila")
+                                + "  Suma: " + rs.getDouble("suma")
+                );
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}   
