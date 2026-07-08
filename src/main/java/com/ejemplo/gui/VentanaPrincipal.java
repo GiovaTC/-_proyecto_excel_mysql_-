640c2b83b8bc6b03ccb7a68b36a5b0bd4cce49e3
Giovanny Alejandro Tapiero Cataño <giovanny.tapiero@pi.edu.co>
2026-07-07 19:30:47 -0500

    commit - VentanaPrincipal .

--- src/main/java/com/ejemplo/gui/VentanaPrincipal.java
+++ src/main/java/com/ejemplo/gui/VentanaPrincipal.java
@@ -2,9 +2,12 @@ package com.ejemplo.gui;
 
 import com.ejemplo.excel.ExcelService;
 import com.ejemplo.util.TablaModelo;
+import org.apache.xmlbeans.impl.xb.xsdschema.ListDocument;
 
 import javax.swing.*;
-import java.awt.*;  
+import java.awt.*;
+import java.io.File;
+import java.util.List;
 
 public class VentanaPrincipal extends JFrame {
 
@@ -96,5 +99,48 @@ public class VentanaPrincipal extends JFrame {
 
         // Se implementará en la Parte 5.2
 
+        JFileChooser selector = new JFileChooser();
+        int opcion = selector.showOpenDialog(this);
+
+        if (opcion == JFileChooser.APPROVE_OPTION) {
+            File archivo = selector.getSelectedFile();
+
+            cargarExcel(archivo);
+        }   
+    }
+
+    private void cargarExcel(File archivo) {
+
+        modelo.setRowCount(0);
+        modelo.setColumnCount(0);
+
+        modelo.addColumn("Columna 1");
+        modelo.addColumn("Columna 2");
+        modelo.addColumn("Columna 3");
+        modelo.addColumn("Columna 4");
+        modelo.addColumn("Suma"); // columna para la suma .
+
+        List<Object[]> datos = excelService.leerExcel(archivo);
+        txtResultados.setText("");
+
+        int fila = 1;
+
+        for (Object[] registro : datos) {
+            modelo.addRow(registro);
+            double suma = (Double) registro[registro.length - 1];
+
+            txtResultados.append(
+                    "Fila " + fila +
+                            " = " + suma +
+                            System.lineSeparator()
+            );
+
+            fila++;
+        }
+
+        JOptionPane.showMessageDialog(
+                this,
+                "Archivo procesado correctamente! "
+        );
     }
 }
